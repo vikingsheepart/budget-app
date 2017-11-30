7c35256d773fafd6d5758bc4160fc781907e1e8e
Lucas Weng <30640930+lucasweng@users.noreply.github.com>
2017-11-30 23:53:13 +0800

    Refactor webpack for heroku

--- webpack.prod.js
+++ webpack.prod.js
@@ -61,6 +61,14 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+    }),
     new CleanWebpackPlugin(['public/dist']),
     // Browsers will do assets caching for vendor
     // 3rd party libs don't change very often, so the load-time will decrease at second visit.
